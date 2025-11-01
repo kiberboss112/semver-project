cd64840cb64786ca6c7a5b73ef5f225cd078e5f7
kiberboss112 <kiberboss112@gmail.com>
2025-11-01 10:24:01 +0300

    1.1.1
    деление на ноль так не должно быть

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ print("Hello, World")
 #v1.0.0 - Наисание Hello world
 print("Как тебя зовут")
 #v1.1.0 - написание как тебя зовут 
+def calculate():
+    result = 10 / 0  
+    return result
+#v1.1.1 - Баг: деление на ноль
